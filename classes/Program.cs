e9f7307ff1f83313f8bbc06860a4d4109f5c5bd0
t.masaki <t-masaki@jpnfootcare.com>
2021-07-14 09:22:53 +0900

    新しいアカウントを開く

--- classes/Program.cs
+++ classes/Program.cs
@@ -6,7 +6,8 @@ namespace classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = new BankAccount("<name>", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
         }
     }
 }
